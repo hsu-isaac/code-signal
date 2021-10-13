80b627b9f03b41d48ad70d2721ce413df0ac663f
Isaac Hsu <16hsuisaac@gmail.com>
2021-10-13 16:34:25 -0700

    completed lastWord algo

---
+++ interview-prep/lastWord.js
@@ -0,0 +1,8 @@
+var lengthOfLastWord = function (s) {
+  const array = s.split(' ');
+  for (let i = array.length - 1; i >= 0; i--) {
+    if (array[i]) {
+      return array[i].length
+    }
+  }
+};
