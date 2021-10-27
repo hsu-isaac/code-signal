daf3decb005fbd6c253b9de829f47cfc5deb7000
Isaac Hsu <16hsuisaac@gmail.com>
2021-10-26 19:21:20 -0700

    solved algo

--- digitDegree.js
+++ digitDegree.js
@@ -1,2 +1,15 @@
 function digitDegree(n) {
+  let count = 0;
+
+  while (n > 9) {
+    count++;
+    let newNum = 0;
+    while (n > 0) {
+      newNum += n % 10;
+      n = (n - (n % 10)) / 10;
+    }
+    n = newNum;
+  }
+
+  return count;
 }
