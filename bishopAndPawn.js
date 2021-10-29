6292e1c1316d2797ccb83f1cf161e70236507b72
Isaac Hsu <16hsuisaac@gmail.com>
2021-10-28 21:28:33 -0700

    solved bishop and pawn algo

--- bishopAndPawn.js
+++ bishopAndPawn.js
@@ -1,2 +1,5 @@
 function bishopAndPawn(bishop, pawn) {
+  let [bCol, bRow] = bishop.split('')
+  let [pCol, pRow] = pawn.split('')
+  return Math.abs(bCol.charCodeAt(0) - pCol.charCodeAt(0)) === Math.abs(bRow - pRow)
 }
