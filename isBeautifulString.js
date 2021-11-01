1c290f584d557ce38fba356ebb2e97667fa2b0e4
Isaac Hsu <16hsuisaac@gmail.com>
2021-11-01 16:38:21 -0700

    solved algo

--- isBeautifulString.js
+++ isBeautifulString.js
@@ -1,3 +1,11 @@
 function isBeautifulString(input) {
+  let arr = new Array(26).fill(0);
+  for (let i = 0; i < input.length; i++) {
+    arr[input[i].charCodeAt(0) - 97]++;
+  }
 
+  for (let i = 0; i < arr.length - 1; i++) {
+    if (arr[i] < arr[i + 1]) return false;
+  }
+  return true;
 }
